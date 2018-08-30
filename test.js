37c81aa527627c3b549f63dd7838b2dfa584c326
Pacheco Castro, J <j.pacheco.castro@accenture.com>
2018-08-30 13:33:35 +0200

    Update with search example

--- test.js
+++ test.js
@@ -6,12 +6,6 @@ let scrape = async () => {
     const page = await browser.newPage();
 
     await page.goto('https://www.packtpub.com/login');
-    //await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
-    //await page.click('#account-bar-login-register > a.login-popup > div');
-    //await page.waitFor('#edit-submit-1', {display: block});
-    //await page.click('#edit-submit-1');
-    //await page.click('#account-bar-form-close > a');
-    //await page.waitFor(5000);
     
     const USERNAME_SELECTOR = '#edit-name';
     const PASSWORD_SELECTOR = '#edit-pass';
@@ -23,13 +17,23 @@ let scrape = async () => {
 
     await page.waitForNavigation();
 
-    const result = await page.evaluate(() => {
+    const SEARCH_TEXT = 'Salesforce';
+    const SEARCH_SELECTOR = '#main-search-keys';
+    const SEARCH_BUTTON_SELECTOR = '#menu-bar-search > span';
+
+    await page.type(SEARCH_SELECTOR, SEARCH_TEXT);
+    await page.click(SEARCH_BUTTON_SELECTOR);
+
+    await page.waitForNavigation();
+    
+    const result = 'Success';
+    /*const result = await page.evaluate(() => {
         let errorMsg = document.querySelector('.messages').innerText;
         return {
             errorMsg
         };
 
-    });
+    });*/
 
     browser.close();
     return result;
