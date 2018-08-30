00f91d2f1f0f5d7f1bfcb0c3a984db7ef3692b88
Pacheco Castro, J <j.pacheco.castro@accenture.com>
2018-08-30 13:13:45 +0200

    Refactor text.js

--- text.js
+++ text.js
@@ -1,4 +1,4 @@
-let transform = async (text) => {
+let transform = async () => {
     const TEXT = `A L E R T A S A C T | V A s
 
 
@@ -11,43 +11,45 @@ let transform = async (text) => {
 
 
 
-    Parnu JK Vaprus 0-3 Paide
-    ESTONIA: MEISTRILllGA
-    28-08-201 8 +0.5 goles 97°/
-    o
+Parnu JK Vaprus 0-3 Paide
+ESTONIA: MEISTRILllGA
+28-08-201 8 +0.5 goles 97°/
+o
     
-    New York Red Bulls 1-0 DC United
-    ESTADOS UNIDOS: MLS
-    27-08-201 8 +0.5 goles 98°/
-    o
+New York Red Bulls 1-0 DC United
+ESTADOS UNIDOS: MLS
+27-08-201 8 +0.5 goles 98°/
+o
     
-    Sporting Cristal 2-1 Sport Rosario
-    PERÚ: TORNEO DESCENTRALIZADO - APERTURA
-    2b-08-201 8 +0.5 goles 98°/
-    o
+Sporting Cristal 2-1 Sport Rosario
+PERÚ: TORNEO DESCENTRALIZADO - APERTURA
+2b-08-201 8 +0.5 goles 98°/
+o
     
-    Aalesund 0-1 Nest-Sotra
-    NORUEGA: OBOS-LIGAEN
-    2b-08-201 8 +0.5 goles 98°/
-    o
+Aalesund 0-1 Nest-Sotra
+NORUEGA: OBOS-LIGAEN
+2b-08-201 8 +0.5 goles 98°/
+o
     
     Molde 2-3 Ranheim
     NORUEGA: ELITESERIEN
     2b-08-201 8 +0.5 goles 98°/
     o`;
     
-    let arr = [];
-    let tmp = TEXT.substring(27, (TEXT.length-1)).replace(/\s/g, " ").split(/\°/g);
-    tmp.pop();
-    tmp.forEach(element => {
-        let e = element.split(/\s\s+/g);
-        e.pop();
-        arr.push({
-            'match' : e[e.length-2],
-            'league' : e[e.length-1]
+    let result = [];
+    let textSplit = TEXT.substring(27, (TEXT.length-1)).replace(/\s/g, " ").split(/\°/g);
+    textSplit.pop();
+    console.log(textSplit);
+    
+    textSplit.forEach(piece => {
+        piece = piece.split(/\s\s+/g);
+        piece.pop();
+        result.push({
+            'match' : piece[piece.length-2],
+            'league' : piece[piece.length-1]
         });
     });
-    return arr;
+    return result;
 }
 
 transform().then((value) => {
